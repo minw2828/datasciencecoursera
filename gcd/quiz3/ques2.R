8f07c3dcbf940afad05ebba9a78f33a9ff06bbe0
minw2828 <san-heng-yi-shu@163.com>
2015-05-28 09:34:01 +1000

    complete quiz3

--- gcd/quiz3/ques2.R
+++ gcd/quiz3/ques2.R
@@ -21,24 +21,52 @@
 ## Description:
 ## This script attempts to answer the above question. 
 ##
+## Answer
+## -15259150 -10575416
+## 
 ## Author:
 ## Min Wang (min.wang@depi.vic.gov.au)
 ##
 ## Date Created:
 ## 27 May 2015
 ##
+## Note:
+## 1. I cannot load jpeg package in BASC, so the analysis was done in R interface, 
+##    Windows based system. 
+## 2. I cannot download the jpg file in a correct form using the download.file() command
+##    under Windows system so I download the file via regular web browser based interface 
+##    approach
+## 3. After the above two corrections, the rest of the code gets the answer correctly.
+## 
 ## Date modified and reason:
 ##
 ## Execution:
 ## Rscript <MODULE_NAME>
 
 
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg"
-download.file(url=fileUrl, destfile="./ques2.raw_data.jpg", method="curl")
-list.files("./")
+install.packages("jpeg")
+
+library(jpeg)
+
+fileUrl  <- "https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg"
+
+# under linux system
+# destfile <- "./ques2.raw_data.jpg"
+# download.file(url=fileUrl, destfile=destfile, method="curl")
+# list.files("./")
+
+# under Windows system
+destfile <- "C:\\Users\\minwang827\\Desktop\\ques2.raw_data.jpg"
+download.file(url=fileUrl, destfile=destfile, method="wininet")
+list.files("C:\\Users\\minwang827\\Desktop")
+
 dateDownload <- date()
 dateDownload
 
-install.packages("jpeg")
+data <- readJPEG(destfile, native=TRUE)
+head(data)
+
+quantile(data, probs=c(0.3, 0.8))
+quantile(data, probs=seq(0,1,0.1))
 
 
