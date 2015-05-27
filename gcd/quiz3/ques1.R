91e956beb2ebc92badefe6ddd8390bed7e434267
minw2828 <san-heng-yi-shu@163.com>
2015-05-27 14:36:16 +1000

    complete gcd quiz3 ques1

--- gcd/quiz3/ques1.R
+++ gcd/quiz3/ques1.R
@@ -24,12 +24,15 @@
 ## 
 ## 59, 460, 474
 ## 25, 36, 45
-## 125, 238,262
+## 125, 238, 262
 ## 403, 756, 798
 ## 
 ## Description:
 ## This script attempts to answer the above question. 
 ##
+## Answer:
+## "125" "238" "262"
+## 
 ## Author:
 ## Min Wang (min.wang@depi.vic.gov.au)
 ##
@@ -41,37 +44,13 @@
 ## Execution:
 ## Rscript <MODULE_NAME>
 
-downloadFileFromWeb <- function(url_link, dest_dir, dest_file) {
-    ## Note: This function is tested working well on 6 May 2015.
-    ## > downloadFileFromWeb("https://d396qusza40orc.cloudfront.net/getda.csv", ".", "ques1.raw_data.csv")
-    ## [1] "File url link is: https://d396qusza40orc.cloudfront.net/getda.csv"
-    ## [1] "Date of download is: Wed May  6 11:36:30 2015"
-    ## [1] "./ques1.raw_data.csv"
-    # check if working directory exists and if it doesn't exist, create one
-    if (! file.exists(dest_dir)) {
-            dir.create(dest_dir)
-        }
-    # download a file from the internet
-    download.file(url=url_link, destfile=dest_file, method="curl")
-    # record the date that the file is downloaded, to keep track in the future 
-    # if the file on the internet has been changed or not over time
-    dataDownloaded <- date()
-    filename <- paste(dest_dir, dest_file, sep='/')
-    if (file.exists(filename)) {
-        print(paste("File url link is:", url_link, sep=" "))
-        print(paste("Date of download is:", dataDownloaded))
-        return(filename)
-    } else {
-        stop(paste("Error: Fail to download file from:", url_link, sep=" "))
-    }
-}
 
-result <- function() {
-    url_link <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
-    raw_file <- downloadFileFromWeb(url_link, '.', 'ques1.raw_data.csv')
-    data     <- read.table(raw_file, sep=",", header=TRUE)
-    agricultureLogical <- data$ACR >= 3 & data$AGS >= 6
-    row.names(head(data[which(agricultureLogical),],3))
-}
+download.file(url="https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv", destfile="./ques1.raw_data.csv", method="curl")
+data <- read.csv("./ques1.raw_data.csv")
+head(data)
+colnames(data)
 
+agricultureLogical <- data$ACR >= 3 & data$AGS >=6
+result <- rownames(data[which(agricultureLogical),][1:3,])
+result
 
