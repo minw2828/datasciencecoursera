33e267434d15091f087dd3a90aed1951c004e1d1
minw2828 <san-heng-yi-shu@163.com>
2015-06-02 17:03:29 +1000

    01.TheDataScientistsToolbox video lectures

--- 01.TheDataScientistsToolbox/video_lectures/week3.video01.TypesOfQuestions.v1.R
+++ 01.TheDataScientistsToolbox/video_lectures/week3.video01.TypesOfQuestions.v1.R
@@ -12,13 +12,57 @@
 ## Goal: Describe a set of data
 ## - The first kind of data analysis performed 
 ## - Commonly applied to census data (普查数据)
-## - a
-## -
-## -
-## -
-##
-##
-##
-##
-##
+##   * census data: 
+##     an official enumeration of the population, with details as to age, sex, 
+##     occupation, etc. (in ancient Rome) the registration of citizens and their 
+##     property, for purposes of taxation. to take a census of (a country, city, 
+##     etc.): The entire nation is censused every 10 years.
+## - The description and interpretation are different steps
+## - Descriptions can usually not be generalized without additional statistical
+##   modeling
 
+## About Exploratory analysis
+## Goal: Find relationships you did not know about
+## - Exploratory models are good for discovering new connections 
+## - They are also useful for defining future studies 
+## - Exploratory analyses are usually not the final say
+## - Exploratory analyses alone should not be used for generalizing/predicting
+## - Correlation does not imply causation
+
+## About Inferential analysis
+## Goal: Use a relatively small sample of data to say something about a bigger
+##       population
+## - Inference (推理) is commonly the goal of statistical models
+## - Inference involves estimating both the quantity you care about and your 
+##   uncertainty about your estiamte
+## - Inference depends heavily on both the population and the sampling scheme
+
+## About Predictive analysis
+## Goal: To use the data on some objects to predict values for another object
+## - If X predicts Y it does not mean that X causes Y
+## - Accurate prediction depends heavily on measuring the right variables
+## - Although there are better and worse prediction models, more data and a 
+##   simple model WORKS REALLY WELL
+## - Prediction is very hard, especially about future REFERENCES
+
+## About Causal analysis
+## Goal: To find out what happens to one variable when you make another variable
+##       change
+## - Usually randomized studies are required to identify causation
+## - There are approaches to inferring causation in non-randomized studies, but 
+##   they are complicated and sensitive to assumptions
+## - Causal relationships are usually identified as average effects, but may 
+##   not apply to every individual 
+## - Causal models are usually the "gold standard" for data analysis
+
+## About Mechanistic analysis (机械分析)
+## Goal: Understand the exact changes in variables that lead to changes in other
+##       variables for individual objects. 
+## - Incredibly hard to infer, excepet in simple situations
+## - Usually modeled by a deterministic set of equations (physical/engineering 
+##   science)
+## - Generally the random component of the data is measurement error
+## - If the equations are known but the parameters are not, they may be infered 
+##   with data analysis
+
+## 
