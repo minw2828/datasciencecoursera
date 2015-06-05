00da4702079f5adcd1df545ca9f470a1aacd5647
minw2828 <san-heng-yi-shu@163.com>
2015-06-05 16:50:49 +1000

    finish 01.TheDataScientistsToolbox/video_lectures/ and quiz

--- 01.TheDataScientistsToolbox/video_lectures/week2.video06.BasicGitCommands.v1.R
+++ 01.TheDataScientistsToolbox/video_lectures/week2.video06.BasicGitCommands.v1.R
@@ -3,29 +3,21 @@
 ## Pushing and pulling
 ## Refer to week2.video06.BasicGitCommands.pdf for workflows of 
 ## Git Data Transport Coommands
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
+
+## Branches 
+## - Sometimes you are workign on a project with a version being used by many people
+## - You may not want to edit that version
+## - so you can create a branch with the command:
+git checkout -b branchname
+## - To see what branch you are on type:
+git branch
+## - to switch back to the master branch type
+git checkout master
+
+## Pull requests
+## - If you fork someone's repo or have multiple branches, you'll both be working 
+##   seperately
+## - Sometimes you want to merge your changes into the other branch/repo
+
 
 
