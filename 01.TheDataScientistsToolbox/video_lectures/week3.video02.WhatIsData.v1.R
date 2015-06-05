a9d552ba3c9cb502ffa1dc9e3f0e2acca6ce61bf
minw2828 <san-heng-yi-shu@163.com>
2015-06-05 18:22:43 +1000

    download lecture videos of 02.RProgramming & 03.GettingAndCleaningData

---
+++ 01.TheDataScientistsToolbox/video_lectures/week3.video02.WhatIsData.v1.R
@@ -0,0 +1,7 @@
+## What is Data?
+
+## The data is the second most important thing
+## The most important thing in data science is the question
+## Often the data will limit or enable the questions
+## But having data cannot save you if you don't have a question
+
--- 01.TheDataScientistsToolbox/video_lectures/week3.video03.WhatAboutBigData.v1.R
+++ 01.TheDataScientistsToolbox/video_lectures/week3.video03.WhatAboutBigData.v1.R
@@ -1,13 +1,7 @@
 ## What About Big Data?
 
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
+## Big or small - you need the right data
+## John Tukey: "The data may not contain the answer. The combination of some data and an
+##   aching desire for an answer does not ensure that a reasonable answer 
+##   can be extracted from a given body of data.."
 
