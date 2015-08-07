f2d56641a06ca2e9c42beb3bab97e437dc219989
minw2828 <san-heng-yi-shu@163.com>
2015-08-07 18:08:27 +1000

    complete assign2

--- 05.ReproducibleResearch/programming_assignments/proj2/proj2.R
+++ 05.ReproducibleResearch/programming_assignments/proj2/proj2.R
@@ -18,7 +18,7 @@
 ## Min Wang (min.wang@ecodev.vic.gov.au)
 ##
 ## Date Created:
-## 31 July 2015
+## 07 August 2015
 ##
 ## Date modified and reason:
 ##
@@ -33,6 +33,7 @@ library(ggplot2)
 library(zoo)
 library(scales)
 library(Hmisc)
+library(reshape2)
 
 
 download.file("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2",
@@ -48,13 +49,14 @@ head(data)
 ## Questions 1
 ## Across the United States, which types of events (as indicated in the EVTYPE 
 ## variable) are most harmful with respect to population health?
-
-
+answer_1    <- ddply(data, .(EVTYPE), summarise, total_fatality=sum(FATALITIES, na.rm=TRUE), total_injuries=sum(INJURIES, na.rm=TRUE))
+table_1     <- answer_1[with(answer_1, order(total_fatality, total_injuries, decreasing=TRUE)), ]
+head(table_1)
 
 ## Question 2
 ## Across the United States, which types of events have the greatest economic 
 ## consequences?
-
-
-
+answer_2 <- ddply(data, .(EVTYPE), summarise, total=sum(PROPDMG+CROPDMG, na.rm=TRUE))
+table_2 <- answer_2[with(answer_2, order(total, decreasing=TRUE)), ]
+ggplot(table_2[1:10, ], aes(x=EVTYPE, y=total)) + geom_bar(stat="identity") + labs(title = "Economic Loss") + labs(x = "Event") + labs(y = "Billion Dollars") + theme(plot.title = element_text(lineheight=.8, face="bold"), axis.text.x=element_text(angle=45,vjust=1,hjust=1))
 
